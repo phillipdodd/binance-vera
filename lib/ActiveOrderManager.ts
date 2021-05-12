3eae73af30aca6e6803da801a15fd852f9584100
Phillip Dodd <phillipdodd@gmail.com>
2021-05-11 22:12:24 -0400

    init

---
+++ lib/ActiveOrderManager.ts
@@ -0,0 +1,28 @@
+import TimerManager from "./TimerManager";
+
+//todo change language of 'state' to 'tradeposition'
+export default class ActiveOrderManager {
+
+    private activeOrders: Map<string, TradePosition>;
+    private timerManager: TimerManager;
+
+    constructor(instance: Instance) {
+        this.activeOrders = new Map<string, TradePosition>();
+        this.timerManager = new TimerManager(instance);
+    }
+
+    public add(orderId: string, tradePosition: TradePosition, tradeSide: TradeSide) {
+        this.activeOrders.set(orderId, tradePosition);
+
+        if (this.timerManager.shouldCreateRelistTimer(tradePosition, tradeSide)) {
+            this.timerManager.createResetTimer(orderId);
+        } else {
+            this.timerManager.createPositionChangeTimer(orderId);
+        }
+    }
+
+    public delete(orderId: string) {
+        this.activeOrders.delete(orderId);
+        this.timerManager.deleteTimer(orderId);
+    }
+}
--- lib/TimerManager.ts
+++ lib/TimerManager.ts
@@ -1,36 +1,5 @@
-interface Instance {
-    toggleTradePosition: Function;
-}
-enum TradePosition { LONG, SHORT }
-enum TradeSide {BUY, SELL}
-
-class ActiveOrderManager {
-
-    private activeOrders: Map<string, TradePosition>;
-    private timerManager: TimerManager;
-
-    constructor(instance: Instance) {
-        this.activeOrders = new Map < string, TradePosition > ();
-        this.timerManager = new TimerManager(instance);
-    }
-
-    public add(orderId: string, tradePosition: TradePosition, tradeSide: TradeSide) {
-        this.activeOrders.set(orderId, tradePosition);
-
-        if (this.timerManager.shouldCreateRelistTimer(tradePosition, tradeSide)) {
-            this.timerManager.createResetTimer(orderId);
-        } else {
-            this.timerManager.createPositionChangeTimer(orderId);
-        }
-    }
-
-    public delete(orderId: string) {
-        this.activeOrders.delete(orderId);
-        this.timerManager.deleteTimer(orderId);
-    }
-}
-
-class TimerManager {
+//todo change language of 'state' to 'tradeposition'
+export default class TimerManager {
 
     private instance: Instance;
     private activeTimers: Map<string, NodeJS.Timeout>;
