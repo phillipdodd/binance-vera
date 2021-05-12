24be92d3efff698a733fcf12b017c1ccc7e4504e
Phillip Dodd <phillipdodd@gmail.com>
2021-05-12 13:37:47 -0400

    removed unnecessary functions

--- lib/Calc.ts
+++ lib/Calc.ts
@@ -40,27 +40,6 @@ export default class Calc {
             throw err;
         }
     }
-    
-    static sum(...decimals: any[]) {
-        try {
-            return this.convertToBigNumbers(...decimals)
-                .reduce((previous, current) => {
-                    return previous.add(current);
-                })
-                .toFixed(8)
-                .valueOf();
-        } catch (err) {
-            throw err;
-        }
-    }
-
-    static getAbsDiff(x: any, y: any, toFixedValue = defaultFixedValue) {
-        try {
-            return math.abs(this.sub(x, y));
-        } catch (err) {
-            throw err;
-        }
-    }
 
     static increaseByPercentage(decimal: any, percentage: any, toFixedValue = CONFIG.DEFAULT_TO_FIXED) {
         try {
