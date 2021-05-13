94e78731c1d5f0f5729936c4c651903f89b9e8aa
Phillip Dodd <phillipdodd@gmail.com>
2021-05-13 15:32:05 -0400

    updated to better use parent class

--- lib/LongPriceController.ts
+++ lib/LongPriceController.ts
@@ -1,6 +1,7 @@
 import { ExecutionReport, OrderSide } from "us-binance-api-node";
 import { PriceController, PriceData } from "./PriceController";
 import Instance from "./Instance";
+import SimplifiedExchangeInfo from "./SimplifiedExchangeInfo";
 
 class LongPriceController extends PriceController  {
 
@@ -10,12 +11,12 @@ class LongPriceController extends PriceController  {
 
     async handleBuy(ex: Partial<ExecutionReport>): Promise<PriceData> {
         const price = await this.instance.getHighestBid(<string>ex.symbol);
-        const quantity = this.instance.getOrderQuantity(price);
+        const quantity = this.getOrderQuantity(price);
         return { price, quantity };
     }
 
     async handleSell(ex: Partial<ExecutionReport>): Promise<PriceData> {
-        const price = await this.instance.getRelistAskPrice(<string>ex.symbol, <string>ex.price, ex.priceLastTrade);
+        const price = await this.getRelistAskPrice(<string>ex.symbol, <string>ex.price, ex.priceLastTrade);
         const quantity = <string>ex.quantity;
         return { price, quantity };
     }
