1468f7255f67e3adbf3339bbb158f877f36cbcc9
Phillip Dodd <phillipdodd@gmail.com>
2021-05-13 15:36:59 -0400

    moved much to order handler and price controller

--- lib/Instance.ts
+++ lib/Instance.ts
@@ -1,22 +1,20 @@
 import 'dotenv/config'
 
 import SimplifiedExchangeInfo from "./SimplifiedExchangeInfo";
-import Calc from "./Calc";
 
 const Binance = require("us-binance-api-node");
-import { ExecutionReport, NewOrder, OrderSide, OutboundAccountInfo, WebSocket } from "us-binance-api-node";
-import { PriceController } from "./PriceController";
-import LongPriceController from "./LongPriceController"
-import ShortPriceController from "./ShortPriceController";
+import { ExecutionReport, OutboundAccountInfo } from "us-binance-api-node";
 import { User, TradePosition, CONFIG, DEFAULTS } from "../constants"
+import OrderHandler from './OrderHandler';
 
-export default class Instance {
+class Instance {
     
-    private client;
+    public readonly client;
+    public readonly exchangeInfo: SimplifiedExchangeInfo;
+    
+    public orderHandler: OrderHandler;
+    
     private tradePosition: TradePosition;
-    private priceControllers: Map<TradePosition, PriceController>;
-    private exchangeInfo: SimplifiedExchangeInfo;
-    private activeOrders: Map<number, TradePosition>;
     private websocketClosers: Map<string, Function>;
 
     constructor(user: User) {
@@ -27,20 +25,15 @@ export default class Instance {
         });
 
         this.tradePosition = DEFAULTS.TRADE_POSITION;
-        this.exchangeInfo = new SimplifiedExchangeInfo(this.client);
 
-        this.priceControllers = new Map();
-        this.priceControllers.set(TradePosition.Long, new LongPriceController(this));
-        this.priceControllers.set(TradePosition.Short, new ShortPriceController(this));
-
-        // {orderId: stateName}
-        this.activeOrders = new Map();
         this.websocketClosers = new Map();
+        this.exchangeInfo = new SimplifiedExchangeInfo(this.client);
+        this.orderHandler = new OrderHandler(this);
     }
 
     async init(): Promise<boolean> {
-        await this.exchangeInfo.init();
         await this.startWebsocket();
+        await this.exchangeInfo.init();
 
         console.log("Instance initialized");
         return true;
@@ -52,6 +45,10 @@ export default class Instance {
         } else {
             this.tradePosition = TradePosition.Long;
         }
+    }
+
+    getTradePosition(): TradePosition {
+        return this.tradePosition;
     }
 
     async startWebsocket(): Promise<void> {
@@ -73,31 +70,6 @@ export default class Instance {
         return this.websocketClosers.size;
     }
 
-    async relistLimitOrder(executionReport: ExecutionReport) {
-        const { symbol, orderId, side } = executionReport;
-        const forPosition = this.getPositionOrderPlacedIn(orderId);
-        const relistSide = reverseSide(side);
-
-        const priceController = <PriceController>this.priceControllers.get(forPosition);
-        const handler = priceController.getHandler(relistSide);
-        const { price, quantity } = await handler(executionReport);
-
-        const relistOrder = await this.placeOrder({
-            symbol,
-            quantity,
-            price,
-            side: <OrderSide>relistSide,
-            type: "LIMIT",
-        });
-
-        this.activeOrders.set(relistOrder.orderId, this.tradePosition);
-        this.activeOrders.delete(executionReport.orderId);
-
-        await this.createTimerForOrder(symbol, relistOrder.orderId);
-
-        return relistOrder;
-    }
-
     createTimerForOrder(symbol: string, orderId: number) {
         setTimeout(async () => {
             const forState = this.activeOrders.get(orderId);
@@ -108,73 +80,12 @@ export default class Instance {
         }, CONFIG.RELIST_TIME);
     }
 
-    getPositionOrderPlacedIn(orderId: number) {
-        const state = this.activeOrders.get(orderId);
-        return state || this.tradePosition;
-    }
-
-    async placeInitOrder(symbol: string, forPosition: TradePosition) {
-        const initSide = getInitOrderSide(forPosition);
-        const priceController = <PriceController>this.priceControllers.get(forPosition);
-        const handler = priceController.getHandler(initSide);
-        const { price, quantity } = await handler({ symbol });
-        const initOrder = await this.placeOrder({
-            symbol,
-            quantity,
-            price,
-            side: <OrderSide>initSide,
-            type: "LIMIT",
-        });
-
-        this.activeOrders.set(initOrder.orderId, forPosition);
-        await this.createTimerForOrder(symbol, initOrder.orderId);
-
-        return initOrder;
-    }
-
-    async placeOrder(options: NewOrder) {
-        const correctedOptions: NewOrder = this.correctTickAndStep(options);
-        const order = await this.client.order(correctedOptions);
-        return order;
-    }
-
-    async cancelOrder(symbol: string, orderId: number) {
-        await this.client.cancelOrder({ symbol, orderId });
-        this.activeOrders.delete(orderId);
-    }
-
     async getOpenOrders(symbol: string) {
         return await this.client.openOrders({ symbol });
     }
 
     async getAccountBalances() {
         return await this.client.accountInfo();
-    }
-
-    async getRelistAskPrice(symbol: string, price: string, priceLastTrade?: string) : Promise<string> {
-        const incomingPrice = priceLastTrade || price;
-        const increasedPrice = this.addTicks(symbol, incomingPrice, DEFAULTS.NUM_TICKS_CHANGED);
-        const lowestAsk = await this.getLowestAsk(symbol);
-        return Math.max(+lowestAsk, +increasedPrice).toString();
-    }
-
-    async getRelistBidPrice(symbol: string, price: string, priceLastTrade?: string) : Promise<string> {
-        const incomingPrice = priceLastTrade || price;
-        const decreasedPrice = this.subTicks(symbol, incomingPrice, DEFAULTS.NUM_TICKS_CHANGED);
-        const highestBid = await this.getHighestBid(symbol);
-        return Math.min(+highestBid, +decreasedPrice).toString();
-    }
-
-    addTicks(symbol: string, price: string, numTicks = DEFAULTS.NUM_TICKS_CHANGED) {
-        const tickSize = this.exchangeInfo.getTickSize(symbol);
-        const increaseAmount = Calc.mul(tickSize, numTicks);
-        return Calc.add(price, increaseAmount);
-    }
-
-    subTicks(symbol: string, price: string, numTicks = DEFAULTS.NUM_TICKS_CHANGED) {
-        const tickSize = this.exchangeInfo.getTickSize(symbol);
-        const decreaseAmount = Calc.mul(tickSize, numTicks);
-        return Calc.sub(price, decreaseAmount);
     }
 
     async getLowestAsk(symbol: string) {
@@ -191,40 +102,6 @@ export default class Instance {
         const orderBook = await this.client.book({ symbol });
         return orderBook;
     }
-
-    getOrderQuantity(price: string): string {
-        return Calc.div(CONFIG.BUYIN, price).toString();
-    }
-
-    correctTickAndStep(options: NewOrder) {
-    //* Market orders will not be including a 'price' property
-    if (options.hasOwnProperty("price")) {
-        options.price = Calc.roundToTickSize(options.price, this.exchangeInfo.getTickSize(options.symbol));
-    }
-
-    if (options.hasOwnProperty("quantity")) {
-        options.quantity = Calc.roundToStepSize(options.quantity, this.exchangeInfo.getStepSize(options.symbol));
-    }
-
-    return options;
-}
 }
 
-function reverseSide(orderSide: OrderSide) {
-    if (orderSide != "BUY" && orderSide != "SELL") {
-        throw new Error(`${orderSide} is not a valid order side`);
-    }
-    if (orderSide === "BUY") {
-        return "SELL";
-    } else {
-        return "BUY"
-    }
-}
-
-function getInitOrderSide(position: TradePosition): OrderSide {
-    if (position === TradePosition.Short) {
-        return "SELL";
-    } else {
-        return "BUY";
-    }
-}
+export default Instance;
--- lib/PriceController.ts
+++ lib/PriceController.ts
@@ -1,8 +1,15 @@
 import { ExecutionReport, OrderSide } from "us-binance-api-node";
+import { CONFIG, DEFAULTS } from "../constants";
+import Calc from "./Calc";
 import Instance from "./Instance";
 abstract class PriceController {
-    constructor(protected instance: Instance) { }
+    constructor(
+        protected instance: Instance
+    ) { }
     
+    abstract handleBuy(ex: Partial<ExecutionReport>): Promise<PriceData>;
+    abstract handleSell(ex: Partial<ExecutionReport>): Promise<PriceData>;
+
     getHandler(side: OrderSide): IPriceHandler {
         if (side === "BUY") {
             return this.handleBuy;
@@ -11,8 +18,36 @@ abstract class PriceController {
         }
     }
 
-    abstract handleBuy(ex: Partial<ExecutionReport>): Promise<PriceData>;
-    abstract handleSell(ex: Partial<ExecutionReport>): Promise<PriceData>;
+    async getRelistAskPrice(symbol: string, price: string, priceLastTrade?: string) : Promise<string> {
+        const incomingPrice = priceLastTrade || price;
+        const increasedPrice = this.addTicks(symbol, incomingPrice, DEFAULTS.NUM_TICKS_CHANGED);
+        const lowestAsk = await this.instance.getLowestAsk(symbol);
+        return Math.max(+lowestAsk, +increasedPrice).toString();
+    }
+
+    async getRelistBidPrice(symbol: string, price: string, priceLastTrade?: string) : Promise<string> {
+        const incomingPrice = priceLastTrade || price;
+        const decreasedPrice = this.subTicks(symbol, incomingPrice, DEFAULTS.NUM_TICKS_CHANGED);
+        const highestBid = await this.instance.getHighestBid(symbol);
+        return Math.min(+highestBid, +decreasedPrice).toString();
+    }
+
+    addTicks(symbol: string, price: string, numTicks = DEFAULTS.NUM_TICKS_CHANGED) {
+        const tickSize = this.instance.exchangeInfo.getTickSize(symbol);
+        const increaseAmount = Calc.mul(tickSize, numTicks);
+        return Calc.add(price, increaseAmount);
+    }
+
+    subTicks(symbol: string, price: string, numTicks = DEFAULTS.NUM_TICKS_CHANGED) {
+        const tickSize = this.instance.exchangeInfo.getTickSize(symbol);
+        const decreaseAmount = Calc.mul(tickSize, numTicks);
+        return Calc.sub(price, decreaseAmount);
+    }
+
+    getOrderQuantity(price: string): string {
+        return Calc.div(CONFIG.BUYIN, price).toString();
+    }
+
 }
 
 type PriceData = {
