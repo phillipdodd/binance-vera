f40821a5dfffb91e9c9c43bdf341f1f358fc5fe5
Phillip Dodd <phillipdodd@gmail.com>
2021-05-17 21:24:59 -0400

    added event types

--- lib/EventSystem/EventType.ts
+++ lib/EventSystem/EventType.ts
@@ -1,6 +1,11 @@
 type EventType =
     "AppInitialized" |
-    "OrderPlaced" |
-    "OrderFilled";
+    "OrderPlaced" | // needs to pass along the used strategy as well
+    "OrderFilled" |
+    "StrategyShouldChange" |
+    "StrategyDidChange" |
+    "OrderShouldCancel" |
+    "OrderCancelled"
+    ;
 
 export default EventType;
