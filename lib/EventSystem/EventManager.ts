1efe4183e34d43ca8e0ca2dc4a8bb92046faa012
Phillip Dodd <phillipdodd@gmail.com>
2021-05-22 05:20:16 -0400

    wip

--- lib/EventSystem/EventManager.ts
+++ lib/EventSystem/EventManager.ts
@@ -1,46 +1,46 @@
 import Instance from "../Instance";
-import EventType from "./EventType";
 import EventListener from "./EventListener";
+import Event from "../Events/Event";
 
 class EventManager {
 
     private instance: Instance;
-    private listeners: Map<EventType, Set<EventListener>>;
+    private listeners: Map<string, Set<EventListener>>;
 
     constructor(instance: Instance) {
         this.instance = instance;
         this.listeners = new Map();
      }
     
-    public subscribe(eventType: EventType, listener: EventListener) {
-        if (!this.listeners.has(eventType)) {
-            this.listeners.set(eventType, new Set());
+    public subscribe(event: Event, listener: EventListener) {
+        if (!this.listeners.has(event.name)) {
+            this.listeners.set(event.name, new Set());
         }
-        this.listeners.get(eventType)?.add(listener);
+        this.listeners.get(event.name)?.add(listener);
     }
 
-    public hasSubscriber(eventType: EventType, listener: EventListener) {
-        return this.listeners.get(eventType)?.has(listener);
+    public hasSubscriber(event: Event, listener: EventListener) {
+        return this.listeners.get(event.name)?.has(listener);
     }
 
-    public unsubscribe(eventType: EventType, listener: EventListener) {
-        this.listeners.get(eventType)?.delete(listener);
+    public unsubscribe(event: Event, listener: EventListener) {
+        this.listeners.get(event.name)?.delete(listener);
     }
 
-    public notify(eventType: EventType, data: any) {
-        this.logEvent(eventType, data)
-        const listenerSet = this.listeners.get(eventType);
+    public notify(event: Event) {
+        this.logEvent(event)
+        const listenerSet = this.listeners.get(event.name);
         listenerSet?.forEach(listener => {
-            listener.update(eventType, data);
+            listener.update(event);
         });
     }
     
-    private logEvent(eventType: EventType, data: any) {
-        if (data && typeof data === "object") {
-            this.instance.logger.debug(`Event: ${eventType} - `);
-            this.instance.logger.debug(data);
+    private logEvent(event: Event) {
+        if (event.args && typeof event.args === "object") {
+            this.instance.logger.debug(`Event: ${event.name} - `);
+            this.instance.logger.debug(event.args);
         } else {
-            this.instance.logger.debug(`Event: ${eventType} - ${data || "null"}`);
+            this.instance.logger.debug(`Event: ${event.name} - ${event.args || "null"}`);
         }
     }
 }
