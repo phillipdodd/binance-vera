4b99426ec7a1c6e370a5babb68da0a46c04c3259
Phillip Dodd <phillipdodd@gmail.com>
2021-05-22 20:24:01 -0400

    moved most of previous instance class logic here

--- app.ts
+++ app.ts
@@ -1,16 +1,31 @@
-import Instance from "./lib/Instance";
+import BinanceMarketplace from "./lib/BinanceMarketplace";
 import { User } from "./constants";
+import SimplifiedExchangeInfo from "./lib/SimplifiedExchangeInfo";
+import WebsocketManager from "./lib/WebsocketManager";
+import EventManager from "./lib/EventSystem/EventManager";
+import OrderHandler from "./lib/OrderHandler";
 
 class App {
     
-    instance: Instance;
+    private binance: BinanceMarketplace;
+    private websocketManager: WebsocketManager;
+    private events: EventManager = EventManager.getEventManager();
+    public orderHandler: OrderHandler;
+
+    public readonly exchangeInfo: SimplifiedExchangeInfo;
 
     constructor(user: User) {
-        this.instance = new Instance(user);
+        this.binance = new BinanceMarketplace(user);
+        this.websocketManager = new WebsocketManager(this.binance);
+        this.exchangeInfo = new SimplifiedExchangeInfo(this.binance);
+        this.orderHandler = new OrderHandler(this.binance, this.exchangeInfo);
     }
 
     async init() {
-        await this.instance.init();
+        await this.exchangeInfo.init();
+        await this.websocketManager.startUserWebsocket();
+
+        this.events.notify("AppInitialized");
     }
 }
 
