112e30a58456ba422bf3e3e205c2c06fddd5b934
Phillip Dodd <phillipdodd@gmail.com>
2021-12-05 17:01:26 -0500

    commented out to not break build

--- lib/maybe not used/Instance.ts
+++ lib/maybe not used/Instance.ts
@@ -1,76 +1,76 @@
-import 'dotenv/config'
-import winston from 'winston';
+// import 'dotenv/config'
+// import winston from 'winston';
 
 
-const Binance = require("us-binance-api-node");
-import { User, DEFAULTS } from "../../constants"
-import AppInitialized from '../Events/AppInitialized';
-import EventManager from '../EventSystem/EventManager';
-import OrderHandler from '../OrderHandler';
-import SimplifiedExchangeInfo from "../SimplifiedExchangeInfo";
-import WebsocketManager from '../WebsocketManager';
+// const Binance = require("us-binance-api-node");
+// import { User, DEFAULTS } from "../../constants"
+// import AppInitialized from '../Events/AppInitialized';
+// import EventManager from '../EventSystem/EventManager';
+// import OrderHandler from '../OrderHandler';
+// import SimplifiedExchangeInfo from "../SimplifiedExchangeInfo";
+// import WebsocketManager from '../WebsocketManager';
 
-class Instance {
+// class Instance {
     
-    public readonly user: User;
-    public readonly client;
-    public readonly events;
-    public readonly exchangeInfo: SimplifiedExchangeInfo;
+//     public readonly user: User;
+//     public readonly client;
+//     public readonly events;
+//     public readonly exchangeInfo: SimplifiedExchangeInfo;
 
-    private logManager: LogManager;
-    public readonly logger: winston.Logger;
+//     private logManager: LogManager;
+//     public readonly logger: winston.Logger;
 
-    public orderHandler: OrderHandler;
+//     public orderHandler: OrderHandler;
 
-    private websocketManager: WebsocketManager;
+//     private websocketManager: WebsocketManager;
 
-    constructor(user: User) {
-        this.user = user;
+//     constructor(user: User) {
+//         this.user = user;
 
-        this.client = Binance.default({
-            apiKey: process.env[`API_KEY_${user}`],
-            apiSecret: process.env[`API_SECRET_${user}`],
-            getTime: Date.now,
-        });
+//         this.client = Binance.default({
+//             apiKey: process.env[`API_KEY_${user}`],
+//             apiSecret: process.env[`API_SECRET_${user}`],
+//             getTime: Date.now,
+//         });
 
-        this.logManager = new LogManager(this);
-        this.logger = this.logManager.logger;
-        this.events = new EventManager(this.logger);
-        this.exchangeInfo = new SimplifiedExchangeInfo(this);
-        this.orderHandler = new OrderHandler(this);
-        this.websocketManager = new WebsocketManager(this);
-    }
+//         this.logManager = new LogManager(this);
+//         this.logger = this.logManager.logger;
+//         this.events = new EventManager(this.logger);
+//         this.exchangeInfo = new SimplifiedExchangeInfo(this);
+//         this.orderHandler = new OrderHandler(this);
+//         this.websocketManager = new WebsocketManager(this);
+//     }
 
-    async init(): Promise<boolean> {
-        await this.exchangeInfo.init();
-        await this.websocketManager.startUserWebsocket();
+//     async init(): Promise<boolean> {
+//         await this.exchangeInfo.init();
+//         await this.websocketManager.startUserWebsocket();
 
-        this.events.notify(new AppInitialized());
-        return true;
-    }
+//         this.events.notify(new AppInitialized());
+//         return true;
+//     }
 
-    async getOpenOrders(symbol: string) {
-        return await this.client.openOrders({ symbol });
-    }
+//     async getOpenOrders(symbol: string) {
+//         return await this.client.openOrders({ symbol });
+//     }
 
-    async getAccountBalances() {
-        return await this.client.accountInfo();
-    }
+//     async getAccountBalances() {
+//         return await this.client.accountInfo();
+//     }
 
-    async getLowestAsk(symbol: string) {
-        const orderBook = await this.getOrderBook(symbol);
-        return orderBook.asks[0].price;
-    }
+//     async getLowestAsk(symbol: string) {
+//         const orderBook = await this.getOrderBook(symbol);
+//         return orderBook.asks[0].price;
+//     }
 
-    async getHighestBid(symbol: string) {
-        const orderBook = await this.getOrderBook(symbol);
-        return orderBook.bids[0].price;
-    }
+//     async getHighestBid(symbol: string) {
+//         const orderBook = await this.getOrderBook(symbol);
+//         return orderBook.bids[0].price;
+//     }
 
-    async getOrderBook(symbol: string) {
-        const orderBook = await this.client.book({ symbol });
-        return orderBook;
-    }
-}
+//     async getOrderBook(symbol: string) {
+//         const orderBook = await this.client.book({ symbol });
+//         return orderBook;
+//     }
+// }
 
-export default Instance;
+// export default Instance;
--- lib/maybe not used/TimerManager.ts
+++ lib/maybe not used/TimerManager.ts
@@ -1,81 +1,81 @@
-import { OrderSide } from "us-binance-api-node";
-import { CONFIG } from "../../constants";
-import OrderPlaced from "../Events/OrderPlaced";
-import EventListener from "../EventSystem/EventListener";
-import EventType from "../EventSystem/EventType";
-import Instance from "./Instance";
-import OrderHandler from "../OrderHandler";
+// import { OrderSide } from "us-binance-api-node";
+// import { CONFIG } from "../../constants";
+// import OrderPlaced from "../Events/OrderPlaced";
+// import EventListener from "../EventSystem/EventListener";
+// import EventType from "../EventSystem/EventType";
+// import Instance from "./Instance";
+// import OrderHandler from "../OrderHandler";
 
-export default class TimerManager implements EventListener {
+// export default class TimerManager implements EventListener {
 
-    private instance: Instance;
-    private activeTimers: Map<string, NodeJS.Timeout>;
-    private orderHandler: OrderHandler;
+//     private instance: Instance;
+//     private activeTimers: Map<string, NodeJS.Timeout>;
+//     private orderHandler: OrderHandler;
 
-    constructor(instance: Instance, orderHandler: OrderHandler) {
-        this.instance = instance;
-        this.orderHandler = orderHandler;
+//     constructor(instance: Instance, orderHandler: OrderHandler) {
+//         this.instance = instance;
+//         this.orderHandler = orderHandler;
 
-        this.instance.events.subscribe(new OrderPlaced(), this);
-        this.instance.events.subscribe("OrderFilled", this);
-        this.instance.events.subscribe("OrderCancelled", this);
+//         this.instance.events.subscribe(new OrderPlaced(), this);
+//         this.instance.events.subscribe("OrderFilled", this);
+//         this.instance.events.subscribe("OrderCancelled", this);
 
-        this.activeTimers = new Map();
-    }
+//         this.activeTimers = new Map();
+//     }
 
-    public update(eventType: EventType, data: any): void {
-        switch (eventType) {
-            case "OrderPlaced":
-                this.onOrderPlaced(data);
-                break;
+//     public update(eventType: EventType, data: any): void {
+//         switch (eventType) {
+//             case "OrderPlaced":
+//                 this.onOrderPlaced(data);
+//                 break;
     
-            case "OrderFilled":
-                this.onOrderFilled(data);
-                break;
+//             case "OrderFilled":
+//                 this.onOrderFilled(data);
+//                 break;
             
-            case "OrderCancelled":
-                this.deleteTimer(data);
-                break;
+//             case "OrderCancelled":
+//                 this.deleteTimer(data);
+//                 break;
             
-            default:
-                break;
-        }
-    }
+//             default:
+//                 break;
+//         }
+//     }
     
-    private onOrderPlaced(data: {side: OrderSide, orderId: string}) {
-        const orderStrategy = this.orderHandler.getStrategy();
-        if (data.side === orderStrategy.startSide) {
-            this.createResetTimer(data.orderId);
-        } else {
-            this.createChangeStrategyTimer(data.orderId);
-        }
-    }
+//     private onOrderPlaced(data: {side: OrderSide, orderId: string}) {
+//         const orderStrategy = this.orderHandler.getStrategy();
+//         if (data.side === orderStrategy.startSide) {
+//             this.createResetTimer(data.orderId);
+//         } else {
+//             this.createChangeStrategyTimer(data.orderId);
+//         }
+//     }
 
-    private onOrderFilled(orderId: string) {
-        this.deleteTimer(orderId);
-    }
+//     private onOrderFilled(orderId: string) {
+//         this.deleteTimer(orderId);
+//     }
 
-    public createResetTimer(orderId: string) {
-        const timer = setTimeout(() => {
-            this.instance.events.notify("OrderShouldCancel", orderId);
-        }, CONFIG.RESET_TIME);
-        this.activeTimers.set(orderId, timer);
-    }
+//     public createResetTimer(orderId: string) {
+//         const timer = setTimeout(() => {
+//             this.instance.events.notify("OrderShouldCancel", orderId);
+//         }, CONFIG.RESET_TIME);
+//         this.activeTimers.set(orderId, timer);
+//     }
 
-    public createChangeStrategyTimer(orderId: string) {
-        const timer = setTimeout(() => {
-            this.instance.events.notify("StrategyShouldChange", null);
-            this.deleteTimer(orderId);
-        }, CONFIG.CHANGE_STRATEGY_TIME);
-        this.activeTimers.set(orderId, timer);
-    }
+//     public createChangeStrategyTimer(orderId: string) {
+//         const timer = setTimeout(() => {
+//             this.instance.events.notify("StrategyShouldChange", null);
+//             this.deleteTimer(orderId);
+//         }, CONFIG.CHANGE_STRATEGY_TIME);
+//         this.activeTimers.set(orderId, timer);
+//     }
 
-    public deleteTimer(orderId: string) {
-        const timeout = this.activeTimers.get(orderId);
-        if (timeout) {
-            clearTimeout(timeout);
-            this.activeTimers.delete(orderId);
-        }
-    }
+//     public deleteTimer(orderId: string) {
+//         const timeout = this.activeTimers.get(orderId);
+//         if (timeout) {
+//             clearTimeout(timeout);
+//             this.activeTimers.delete(orderId);
+//         }
+//     }
 
-}
+// }
