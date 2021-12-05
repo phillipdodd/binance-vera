28d59346856bb4e820abcdd97b07f22ad826ed96
Phillip Dodd <phillipdodd@gmail.com>
2021-12-05 14:21:07 -0500

    moved order and websocket funcs here

--- lib/BinanceMarketplace.ts
+++ lib/BinanceMarketplace.ts
@@ -1,25 +1,31 @@
-import winston from 'winston';
+import { Binance, CancelOrderResult, ExecutionReport, NewOrder, OutboundAccountInfo } from "us-binance-api-node";
 import { User } from "../constants";
-import LogManager from './LogManager';
-const Binance = require("us-binance-api-node");
+import OrderFilled from "./Events/OrderFilled";
+import EventManager from "./EventSystem/EventManager";
+import SimplifiedExchangeInfo from "./SimplifiedExchangeInfo";
+const binance = require("us-binance-api-node");
 
 class BinanceMarketplace {
     
     public readonly user: User;
-    public readonly client;
+    public readonly client: Binance;
+    
+    public events: EventManager;
+    public exchangeInfo: SimplifiedExchangeInfo;
 
-    private logger: winston.Logger;
+    private websocketClosers: Map<string, Function> = new Map();
 
-    constructor(user: User) {
+    constructor(user: User, events: EventManager) {
         this.user = user;
+        this.events = events;
+        
+        this.exchangeInfo = new SimplifiedExchangeInfo(this);
 
-        this.client = Binance.default({
+        this.client = binance.default({
             apiKey: process.env[`API_KEY_${user}`],
             apiSecret: process.env[`API_SECRET_${user}`],
             getTime: Date.now,
         });
-
-        this.logger = LogManager.getLogger();
     }
 
     async getOpenOrders(symbol: string) {
@@ -43,6 +49,43 @@ class BinanceMarketplace {
     async getOrderBook(symbol: string) {
         const orderBook = await this.client.book({ symbol });
         return orderBook;
+    }
+
+    public async placeOrder(orderOptions: NewOrder) {
+        const orderResponse = await this.client.order(orderOptions);
+        return orderResponse;
+    }
+
+    public async cancelOrder(symbol: string, orderId: string | number): Promise<CancelOrderResult> {
+        const cancelOrderResult = await this.client.cancelOrder({ symbol, orderId: <number>orderId });
+        return cancelOrderResult;
+    }
+
+    async startUserWebsocket(): Promise<void> {
+        const isExecutionReport = (eventData: OutboundAccountInfo | ExecutionReport) : eventData is ExecutionReport => {
+            return (eventData as any).orderStatus !== undefined;
+        }
+
+        const userCallback = (eventData: OutboundAccountInfo | ExecutionReport) => {
+            if (isExecutionReport(eventData)) {
+                if (eventData.orderStatus === 'FILLED') {
+                    const orderFilledEvent = new OrderFilled(eventData);
+                    this.events.notify(orderFilledEvent);
+                }
+            }
+        }
+
+        const userWebsocketCloser = await this.client.ws.user(userCallback);
+        this.websocketClosers.set('user', userWebsocketCloser);
+    }
+
+    closeAllWebsockets() {
+        this.websocketClosers.forEach(ws => ws());
+        this.websocketClosers.clear();
+    }
+
+    getOpenWebsocketCount(): number {
+        return this.websocketClosers.size;
     }
 }
 
