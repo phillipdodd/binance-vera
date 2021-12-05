02511f84a714a23dedeb63452633aaffbf5da610
Phillip Dodd <phillipdodd@gmail.com>
2021-12-05 16:59:43 -0500

    subscribes; calls handlers correctly

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-import winston from "winston";
+import winston, { loggers } from "winston";
 import WinstonLogger from "./lib/WinstonLogger";
 import { User, USER_CONFIG } from "./constants";
 import BinanceMarketplace from "./lib/BinanceMarketplace";
@@ -25,20 +25,32 @@ class App implements EventListener {
         this.user = user;
         this.marketplace = new BinanceMarketplace(user, this.events);
 
+        this.events.subscribe("AppInitialized", this);
         this.eventHandlers.set("AppInitialized", this.onAppInitialized)
+
+        this.events.subscribe("OrderFilled", this);
         this.eventHandlers.set("OrderFilled", this.onOrderFilled)
+        
+        this.events.subscribe("OrderShouldCancel", this);
         this.eventHandlers.set("OrderShouldCancel", this.onOrderShouldCancel)
+        
+        this.events.subscribe("StrategyShouldChange", this);
         this.eventHandlers.set("StrategyShouldChange", this.onStrategyShouldChange)
+    }
+
+    async init() {
+        this.marketplace.init();
     }
 
     update(event: Event): void {
         const handler = this.eventHandlers.get(event.name)
         if (handler) {
-            handler(event);
+            handler.apply(this, [event]);
         }
     }
 
     private async onAppInitialized() {
+        console.log('onAppInitialized')
         const initSymbols = USER_CONFIG[this.user].INIT_SYMBOLS;
         console.log('in testing mode! onAppInitialized() triggered')
         for (const symbol of initSymbols) {
@@ -72,3 +84,8 @@ class App implements EventListener {
         this.marketplace.setStrategy(targetStrategy);
     }
 }
+
+(async () => {
+    const app = new App(User.Phil);
+    await app.init();
+})()
