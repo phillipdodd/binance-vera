73b8a393345de56be9f9fa10614381415dd5b4e8
Phillip Dodd <phillipdodd@gmail.com>
2022-02-19 14:31:40 -0500

    updated to use ExecutionReport

--- lib/Events/OrderCancelled.ts
+++ lib/Events/OrderCancelled.ts
@@ -1,18 +1,17 @@
-import { CancelOrderResult } from "us-binance-api-node";
+import { ExecutionReport } from "us-binance-api-node";
 import Event from "./Event";
 
 type OrderCancelledArgs = {
-    cancelOrderResult: CancelOrderResult
+    executionReport: ExecutionReport
 }
-
 
 //todo this should be named OrderDidCancel
 class OrderCancelled implements Event {
     public readonly name: string = "OrderCancelled"
     public readonly args: OrderCancelledArgs;
 
-    constructor(cancelOrderResult: CancelOrderResult) {
-        this.args = { cancelOrderResult };
+    constructor(executionReport: ExecutionReport) {
+        this.args = { executionReport };
     }
 }
 
